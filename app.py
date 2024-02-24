5d7f50fe5930e080629e96861d3a801accf20ca0
Andrew Miller <andrewmillerfirm@gmail.com>
2024-02-24 15:38:50 -0800

    d

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, request,render_template
+import requests
+from bs4 import BeautifulSoup
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/process', methods=['POST']) 
+def process():
+    response = requests.get('https://pastebin.com/Xe9bsnF1')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    key = soup.find("div", class_ ="de1").text
+    data = key.strip()
+    # process the data using Python code 
+    result = data.upper() 
+    return result 
+
+
