83c767528d5eb3da49741b6ff49d28e964bd7304
Andrew Miller <andrewmillerfirm@gmail.com>
2024-02-24 15:49:02 -0800

    new

--- api/app.py
+++ api/app.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
-
+'''
 @app.route('/process', methods=['POST']) 
 def process():
-    response = requests.get('https://pastebin.com/Xe9bsnF1')
-    soup = BeautifulSoup(response.text, 'html.parser')
-    key = soup.find("div", class_ ="de1").text
-    data = key.strip()
+    #response = requests.get('https://pastebin.com/Xe9bsnF1')
+    #soup = BeautifulSoup(response.text, 'html.parser')
+    #key = soup.find("div", class_ ="de1").text
+    #data = key.strip()
     # process the data using Python code 
-    result = data.upper() 
+    result = 'skull'
     return result 
-
+'''
 
