3ae70be3f01749ab2edceb84c08b47b21250e3ba
Andrew Miller <andrewmillerfirm@gmail.com>
2024-02-24 17:03:44 -0800

    get key

--- api/app.py
+++ api/app.py
@@ -12,8 +12,10 @@ def index():
     return render_template('index.html')
 @app.route("/flaskFunction")
 def returnSimpleFlaskFunc():
-    i = 12.2+8129.4
-    return str(i)
+    response = requests.get('https://pastebin.com/Xe9bsnF1')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    key = soup.find("div", class_ ="de1").text
+    return str(key.strip())
 
 if __name__ == "__main__":
     serve(app,host='0.0.0.0',port=50100)
