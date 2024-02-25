6c4d994e64fe45d461275ade7be760d53f7586d7
Andrew Miller <andrewmillerfirm@gmail.com>
2024-02-24 16:30:05 -0800

    ready

--- api/app.py
+++ api/app.py
@@ -10,16 +10,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
-'''
-@app.route('/process', methods=['POST']) 
-def process():
-    #response = requests.get('https://pastebin.com/Xe9bsnF1')
-    #soup = BeautifulSoup(response.text, 'html.parser')
-    #key = soup.find("div", class_ ="de1").text
-    #data = key.strip()
-    # process the data using Python code 
-    result = 'skull'
-    return result 
-'''
+@app.route("/flaskFunction")
+def returnSimpleFlaskFunc():
+    i = 12.2+8129.4
+    return str(i)
+
 if __name__ == "__main__":
     serve(app,host='0.0.0.0',port=50100)
